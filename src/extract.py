55e6c6faf004e50124e2859170584cc04e6e3709
dcrauwels <d.crauwels@gmail.com>
2024-04-27 18:21:10 +0200

    Wrote a short set of regex-based functions for extracting MD formatted hyperlinks and images in extract.py.

---
+++ src/extract.py
@@ -0,0 +1,27 @@
+import re
+# Don't think I need these two libraries yet.
+#import textnode
+#import htmlnode
+
+def extract_markdown_images(text: str) -> list:
+    # Returns a list of tuples containing (alt text, image url). In markdown, an image is formatted as ![ALT](IMAGE_URL).
+    # Will use regex to extract the alt text and url. Regex string should detect:
+    # 1. !
+    # 2. [
+    # 3. Capture group 1: alt text
+    # 4. ]
+    # 5. (
+    # 6. Capture group 2: image url
+    # 7. )
+    # In summary: !\[(.*?)\]\((.*?)\)
+    
+    reg_str = r"!\[(.*?)\]\((.*?)\)"
+    return re.findall(reg_str, text)
+
+def extract_markdown_links(text: str) -> list:
+    # Returns a list of tuples containing (anchor text, url). In markdown, a hyperlink is formatted as [ANCHOR_TEXT](URL).
+    # Will use regex to extract the alt text and url. Based on previous, regex string should be:
+    # \[(.*?)\][((.*?)\)
+
+    reg_str = r"!\[(.*?)\]\((.*?)\)"
+    return re.findall(reg_str, text)
