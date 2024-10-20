b70abe646b0c80ddfa9b8456b13392ba598ef49a
dcrauwels <d.crauwels@gmail.com>
2024-10-20 12:52:46 +0200

    copy static to public function complete

--- src/copy_static.py
+++ src/copy_static.py
@@ -1,41 +1,43 @@
 import os, shutil
 
-def list_static_contents(destination = "static") -> list:
+def list_static_contents(source = "static") -> list:
     # list contents of static dir with recursive call, replacing dir with (dir, contents) tuple
-    # returns a list
 
     # escape
-    if not os.path.exists(destination):
+    if not os.path.exists(source):
         raise OSError(f"Destination {destination} does not exist.")
 
     # build contents list
-    contents = [os.path.join(destination, item) for item in os.listdir(destination)]
-    for (i, c) in enumerate(contents):
-        if os.path.isdir(c):
+    contents = [os.path.join(source, item) for item in os.listdir(source)]
+    for (i, c) in enumerate(contents): # iterate over files in source dir and if a dir is found, call recursively
+        if os.path.isdir(c): # (could probably do this via list comprehension in one line?)
             contents[i] = (c, list_static_contents(c)) # recursive call if directory found
 
     return contents
+
 
 def strip_leading_destination(destination: str) -> str:
     # strip the leading destination from the destinations provided by list_static_contents()
     # called individually depending on dir or file in copy_static_contents()
     # usually removes "static/" but fundamentally agnostic
+
     path_list = destination.split("/") # this is not platform agnostic I think
     return "/".join(path_list[1:])
     
 
-def copy_static_contents(contents: list, destination = "../public") -> None:
-    # copy static dir in src dir to public dir in root dir
+def copy_static_contents(contents: list, destination = "public") -> None:
+    # copy ./static dir to ./public dir
 
     # escapes
-    if len(contents) == 0: # in case contents are empty
+    if len(contents) == 0: # in case contents are empty we escape
         return
-    if os.path.exists(destination): # idempotency (this is sus) 
+
+    # idempotency
+    if os.path.exists(destination): # remove destination (default public) dir if it already exists
         shutil.rmtree(destination)
     os.mkdir(destination)
 
-
-    # iterate over contents, copying per item, checking if  dir or file
+    # iterate over contents, copying per item, checking if c is dir or file
     for c in contents:
         
         if type(c) == tuple: # this means it's a dir
---
+++ src/generate_page.py
@@ -0,0 +1,7 @@
+def extract_title(markdown: str) -> str:
+    pass
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    # Generates an html page based on a markdown file in from_path and a template html document in html_path.
+
+    pass
